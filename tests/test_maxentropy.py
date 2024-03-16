3c84adbaf118ed74b2a76b572862216fa3bc09b3
Ed Schofield <ed@pythoncharmers.com>
2024-03-16 11:05:31 +1100

    Add LICENSE.md

--- tests/test_maxentropy.py
+++ tests/test_maxentropy.py
@@ -4,31 +4,25 @@
 Test functions for maximum entropy module.
 """
 
-from numpy.testing import assert_almost_equal, TestCase, run_module_suite
-from numpy import arange, log, exp, ones
-from scipy.maxentropy.maxentropy import logsumexp
+from numpy import arange, log, exp, ones, isclose
+from scipy_maxentropy import logsumexp
 
 
-class TestMaxentropy(TestCase):
+def test_logsumexp(self):
     """Test whether logsumexp() function correctly handles large
     inputs.
     """
 
-    def test_logsumexp(self):
-        a = arange(200)
-        desired = log(sum(exp(a)))
-        assert_almost_equal(logsumexp(a), desired)
+    a = arange(200)
+    desired = log(sum(exp(a)))
+    assert isclose(logsumexp(a), desired)
 
-        # Now test with large numbers
-        b = [1000, 1000]
-        desired = 1000.0 + log(2.0)
-        assert_almost_equal(logsumexp(b), desired)
+    # Now test with large numbers
+    b = [1000, 1000]
+    desired = 1000.0 + log(2.0)
+    assert isclose(logsumexp(b), desired)
 
-        n = 1000
-        b = ones(n) * 10000
-        desired = 10000.0 + log(n)
-        assert_almost_equal(logsumexp(b), desired)
-
-
-if __name__ == "__main__":
-    run_module_suite()
+    n = 1000
+    b = ones(n) * 10000
+    desired = 10000.0 + log(n)
+    assert isclose(logsumexp(b), desired)
